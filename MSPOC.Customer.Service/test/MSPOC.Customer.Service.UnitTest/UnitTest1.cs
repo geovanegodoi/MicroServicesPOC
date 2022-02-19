ae94544f2c554cde0990e399613850a83e8383b3
Geovane Godoi <geovane.godoi@gmail.com>
2022-02-19 20:57:39 -0300

    Change project structure to add test project

---
+++ MSPOC.Customer.Service/test/MSPOC.Customer.Service.UnitTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace MSPOC.Customer.Service.UnitTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
